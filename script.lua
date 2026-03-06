8b63b8110cefc1aec5c751294bc853b326547f4a
shoya <iamshouryakotian@gmail.com>
2026-03-06 23:51:41 +0530

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
--- Load Rayfield
-local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+-- load rayfield
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Players = game:GetService("Players")
 local UIS = game:GetService("UserInputService")
@@ -9,10 +9,7 @@ local player = Players.LocalPlayer
 local speedValue = 16
 local speedEnabled = false
 
----
-
-## -- WINDOW
-
+-- window
 local Window = Rayfield:CreateWindow({
 Name = "JenniferW v1.2",
 LoadingTitle = "JenniferW v1.2",
@@ -20,20 +17,14 @@ LoadingSubtitle = "by Shoya",
 ConfigurationSaving = {Enabled = false}
 })
 
----
-
-## -- TABS
-
+-- tabs
 local MainTab = Window:CreateTab("Main", 4483362458)
 local SettingsTab = Window:CreateTab("Settings", 4483362458)
 local InfoTab = Window:CreateTab("Info", 4483362458)
 
----
-
-## -- NOTIFICATION
-
+-- notification
 Rayfield:Notify({
-Title = "JenniferW v1.2",
+Title = "JenniferW V1.2",
 Content = "Loaded!",
 Duration = 4
 })
@@ -64,21 +55,16 @@ speedEnabled = state
 end
 })
 
--- Keep speed applied
+-- enforce speed
 task.spawn(function()
 while true do
 task.wait(0.1)
-
-```
-  if speedEnabled then
-     local char = player.Character
-
-     if char and char:FindFirstChild("Humanoid") then
-        char.Humanoid.WalkSpeed = speedValue
-     end
-  end
-```
-
+if speedEnabled then
+local char = player.Character
+if char and char:FindFirstChild("Humanoid") then
+char.Humanoid.WalkSpeed = speedValue
+end
+end
 end
 end)
 
@@ -95,20 +81,29 @@ end
 
 ---
 
+## -- UI TOGGLE
+
+UIS.InputBegan:Connect(function(input, gp)
+if input.KeyCode == Enum.KeyCode.Zero then
+Rayfield:Toggle()
+end
+end)
+
+---
+
 ## -- INFO TAB
 
--- Username
-InfoTab:CreateLabel("Username: " .. player.Name)
+local player = game.Players.LocalPlayer
 
--- Avatar
+InfoTab:CreateLabel("Username: "..player.Name)
+
 local userId = player.UserId
 
 InfoTab:CreateImage({
 Name = "User Avatar",
-Image = "https://www.roblox.com/headshot-thumbnail/image?userId=" .. userId .. "&width=420&height=420&format=png"
+Image = "https://www.roblox.com/headshot-thumbnail/image?userId="..userId.."&width=420&height=420&format=png"
 })
 
--- Runtime Stopwatch
 local startTime = tick()
 
 local runtimeLabel = InfoTab:CreateLabel("Running Time: 0s")
@@ -118,21 +113,11 @@ while true do
 task.wait(1)
 
 ```
-  local elapsed = math.floor(tick() - startTime)
+    local elapsed = math.floor(tick() - startTime)
 
-  runtimeLabel:Set("Running Time: " .. elapsed .. "s")
+    runtimeLabel:Set("Running Time: "..elapsed.."s")
+end
 ```
 
-end
-end)
-
----
-
-## -- UI TOGGLE KEY
-
-UIS.InputBegan:Connect(function(input, gp)
-if input.KeyCode == Enum.KeyCode.Zero then
-Rayfield:Toggle()
-end
 end)
 
